21de2198b8d275acea7b423f23978db8d00715da
Caro-vcg <42705387+Caro-vcg@users.noreply.github.com>
2019-02-07 09:34:40 -0600

    Create Reto3.js

---
+++ Reto3.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noStroke();
+  fill(255,0,0);
+  ellipse(200,200,300,300);
+  fill(255);
+  ellipse(200,200,255,255);
+  fill(255,0,0);
+  strokeWeight(25);
+  stroke(255,0,0);
+  line(105,105,290,290);
+  
+}
